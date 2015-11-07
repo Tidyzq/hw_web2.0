e8eb67f8b53ab902dd6ae24cba4d515cbd61ce9d
Tidyzq <493876758@qq.com>
2015-11-07 18:27:31 +0800

    hw_5
    finished hw_5

--- hw_5/calculator.js
+++ hw_5/calculator.js
@@ -3,11 +3,10 @@ window.onload = function () {
     var resultView = document.getElementById("result-view");
     var expressionView = document.getElementById("expression-view");
     var expressionArr = [];
-    var inputNumber = false; // whether the user just inputed number
+    var resultArr = [];
+    var inputNumber = false;
     var showSubResult = false; // whether the resultView is showing a sub-result
     var showResult = false; // whether the resultView is showing a result
-    var leftBracket = false; // whether the user just inputed a left bracket
-    var rightBracket = false; // whether the user just inputed a right bracket
     var brackets = 0; // count the number of unmatched left brackets
 
     var numberMap = {
@@ -35,14 +34,128 @@ window.onload = function () {
     // init calculator
     clearResult();
 
+    var expressionType = {
+        none: -1, number: 0, operation: 1, leftBracket: 2, rightBracket: 3, negative: 4
+    }
+
+    function lastExpression() {
+        if (expressionArr.length) {
+            return expressionArr[expressionArr.length - 1];
+        }
+        return "";
+    }
+
+    function preLastExpression() {
+        if (expressionArr.length > 1) {
+            return expressionArr[expressionArr.length - 2];
+        }
+        return "";
+    }
+
+    function replaceLastExpression(expression) {
+        if (expressionArr.length) {
+            expressionArr[expressionArr.length - 1] = expression;
+        }
+    }
+
+    function getExpressionType(expression, preExpression) {
+        var re = new RegExp("[0-9.]{1,}");
+        if (re.test(expression)) return expressionType.number;
+        switch (expression) {
+            case "+": case "*": case "/":
+                return expressionType.operation;
+                break;
+            case "-":
+                var preType = getExpressionType(preExpression, "");
+                if (preType == expressionType.none || preType == expressionType.operation || preType == expressionType.rightBracket) {
+                    return expressionType.negative;
+                } else {
+                    return expressionType.operation;
+                }
+                break;
+            case "(":
+                return expressionType.leftBracket;
+                break;
+            case ")":
+                return expressionType.rightBracket;
+        }
+        return expressionType.none;
+    }
+
     function refreshExpression() {
         expressionView.value = expressionArr.join(" ");
         // expressionView.style.fontSize = Math.min(16, 800 / expressionView.value.length) + "px";
     }
 
+    function addExpression(add) {
+        var addType = getExpressionType(add, lastExpression());
+        var lastType = getExpressionType(lastExpression(), preLastExpression());
+        switch (addType) {
+            case expressionType.number: {
+                if (lastType == expressionType.rightBracket) {
+                    expressionArr.push("*");
+                    resultArr.push("*");
+                }
+                expressionArr.push(add);
+                resultArr.push(add);
+                break;
+            }
+            case expressionType.operation: {
+                switch (lastType) {
+                    case expressionType.number: case expressionType.rightBracket: {
+                        expressionArr.push(add);
+                        resultArr.push(add);
+                        break;
+                    }
+                    case expressionType.operation: {
+                        replaceLastExpression(add);
+                        resultArr[resultArr.length - 1] = add;
+                        break;
+                    }
+                }
+                break;
+            }
+            case expressionType.leftBracket: {
+                switch (lastType) {
+                    case expressionType.number: case expressionType.rightBracket: {
+                        expressionArr.push("*");
+                        resultArr.push("*");
+                        break;
+                    }
+                }
+                expressionArr.push(add);
+                resultArr.push(add);
+                ++brackets;
+                break;
+            }
+            case expressionType.rightBracket: {
+                if (brackets) {
+                    switch (lastType) {
+                        case expressionType.number: case expressionType.rightBracket: {
+                            expressionArr.push(add);
+                            resultArr.push(add);
+                            --brackets;
+                            calSubResult();
+                        }
+                    }
+                }
+                break;
+            }
+            case expressionType.negative: {
+                expressionArr.push(add);
+                resultArr.push(add);
+                break;
+            }
+        }
+        refreshExpression();
+    }
+
     function refreshResult(str) {
         resultView.value = str;
         resultView.style.fontSize = Math.min(60, 630 / resultView.value.length) + "px";
+        showSubResult = false;
+        showResult = false;
+        inputNumber = true;
     }
 
     function resultAdd(str) {
@@ -54,94 +167,94 @@ window.onload = function () {
     // clear resultView
     function clearResult() {
         refreshResult("0");
+        inputNumber = false;
         showSubResult = false;
         showResult = false;
     }
 
+    function calSubResult() {
+        for (var i = 0; i < resultArr.length; ++i) {
+            if (getExpressionType(resultArr[i], i ? resultArr[i - 1] : "") == expressionType.negative && getExpressionType(resultArr[i + 1], "") == expressionType.number) {
+                var number = -eval(resultArr[i + 1])
+                resultArr = resultArr.slice(0, i);
+                resultArr.push(number);
+            }
+        }
+        if (resultArr.length && resultArr[resultArr.length - 1] == ")") {
+            var i = resultArr.lastIndexOf("(");
+            var sub = resultArr.slice(i, resultArr.length);
+            resultArr = resultArr.slice(0, i);
+            resultArr.push(eval(eval(sub.join("")).toFixed(8)));
+            calSubResult();
+        }
+        var flag = false;
+        for (var i = 0; i < resultArr.length; ++i) {
+            if (resultArr[i] == "*" || resultArr[i] == "/") {
+                if (getExpressionType(resultArr[i - 1], "") == expressionType.number && getExpressionType(resultArr[i + 1], "") == expressionType.number) {
+                    var sub = resultArr.slice(i - 1, resultArr.length);
+                    resultArr = resultArr.slice(0, i - 1);
+                    resultArr.push(eval(eval(sub.join("")).toFixed(8)));
+                    flag = true;
+                }
+            }
+        }
+        if (flag) calSubResult();
+        console.log("resultArr:" + resultArr.join(""), 0);
+    }
+
     function addNumber() {
         var str = resultView.value;
-        if (str.indexOf(".", 0) == str.length - 1) {
-            str = str.substr(0, str.length - 1);
-            refreshResult(str);
+        var i = str.length - 1;
+        while (i > 0 && (str[i] == "." || str[i] == "0")) {
+            --i;
         }
-        expressionArr.push(str);
+        str = str.substring(0, i + 1);
+        addExpression(str);
+        calSubResult();
+        resultView.value = resultArr[resultArr.length - 1];
         showSubResult = true;
+        inputNumber = false;
     }
 
     // clear all
     function clear() {
         clearResult();
         expressionArr = [];
+        resultArr = [];
         expressionView.value = "";
         brackets = 0;
-        inputNumber = false;
         showSubResult = false;
         showResult = false;
-        leftBracket = false;
-        rightBracket = false;
     }
 
     document.getElementById("zero").onclick = function () {
-        // if just inputed right bracket, then add a *
-        if (rightBracket) {
-            expressionArr.push("*");
-            rightBracket = false;
-        }
-        // if the resultView is showing result, then clear the resultView
         if (!showResult && !showSubResult && resultView.value != "0") {
             resultAdd("0")
         } else {
-            clearResult();
+            refreshResult("0");
         }
-        inputNumber = true;
-        leftBracket = false;
     };
 
     for (var key in numberMap) {
         document.getElementById(key).onclick = function () {
-            // if just inputed right bracket, then add a *
-            if (rightBracket) {
-                expressionArr.push("*");
-                rightBracket = false;
-            }
             // if the number in resultView is 0, then replace it with 1, otherwise add 1 in the back
             if (resultView.value != "0" && !showResult && !showSubResult) {
                 resultAdd(numberMap[this.id]);
             } else {
                 refreshResult(numberMap[this.id]);
             }
-            inputNumber = true;
-            showSubResult = false;
-            showResult = false;
-            leftBracket = false;
         }
     }
 
     document.getElementById("left-bracket").onclick = function () {
-        // if justed inputed number, then add a (
-        if (rightBracket) {
-            expressionArr.push("*");
-            rightBracket = false;
-        }
-        expressionArr.push("(")
-        refreshExpression();
-        ++brackets;
-        leftBracket = true;
+        addExpression("(");
     };
 
     document.getElementById("right-bracket").onclick = function () {
-        if (brackets > 0 && (showSubResult || showResult || inputNumber)) {
-            if (!rightBracket) {
-                addNumber();
-            }
-            expressionArr.push(")");
-            refreshExpression();
-            --brackets;
-            inputNumber = true;
-            showSubResult = true;
-            showResult = false;
-            rightBracket = true;
+        if (inputNumber) {
+            addNumber();
         }
+        addExpression(")");
     }
 
     document.getElementById("backspace").onclick = function onBackSpace() {
@@ -153,16 +266,11 @@ window.onload = function () {
     };
 
     document.getElementById("point").onclick = function () {
-        if (rightBracket) {
-            expressionArr.push("*");
-            rightBracket = false;
-        }
         if (showSubResult || showResult) {
             clearResult();
         }
         if (resultView.value.indexOf(".", 0) == -1) {
-            resultView.value += ".";
-            inputNumber = true;
+            resultAdd(".");
         }
     };
 
@@ -173,38 +281,29 @@ window.onload = function () {
     for (var key in operationMap) {
         document.getElementById(key).onclick = function () {
             if (inputNumber || showResult) {
-                if (!rightBracket) {
-                    addNumber();
-                }
-                expressionArr.push(operationMap[this.id]);
-                refreshExpression();
-                inputNumber = false;
-                showSubResult = true;
-                showResult = false;
-                leftBracket = false;
-                rightBracket = false;
-            } else if (!rightBracket && !leftBracket && expressionArr.length) {
-                expressionArr.pop();
-                expressionArr.push(operationMap[this.id]);
-                refreshExpression();
+                addNumber();
             }
+            addExpression(operationMap[this.id]);
+            inputNumber = false;
         }
     }
 
     document.getElementById("equal").onclick = function () {
         if (inputNumber) {
-            if (!rightBracket) {
-                addNumber();
-            }
+            addNumber();
+        }
+        var lastType = getExpressionType(lastExpression(), preLastExpression());
+        if (lastType == expressionType.number || lastType == expressionType.rightBracket) {
             while (brackets) {
-                expressionArr.push(")");
+                resultArr.push(")");
+                calSubResult();
                 --brackets;
             }
             // sync check
             try {
-                var result = eval(expressionArr.join(""));
+                var result = eval(resultArr.join(""));
                 clear();
-                refreshResult(result);
+                refreshResult(eval(result.toFixed(8)));
                 showResult = true;
             }
             catch (exception) {
