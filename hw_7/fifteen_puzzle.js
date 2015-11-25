81930581032c1cc830e9b0154908b6e551302173
Tidyzq <493876758@qq.com>
2015-11-25 22:10:50 +0800

    reconsituted hw_7

--- hw_7/fifteen_puzzle.js
+++ hw_7/fifteen_puzzle.js
@@ -1,6 +1,9 @@
 var puzzle = {
     matrix: new Array(),
+    blocks: new Array(),
     randomTimes: 100,
+    randomDelay: 5,
+    solveDelay: 50,
     gameStart: false,
     ableToClick: true,
     gameTime: 0,
@@ -8,15 +11,15 @@ var puzzle = {
     gameMoves: 0,
     clearTime: function () {
         puzzle.gameTime = 0;
-        document.getElementById("time_counter").textContent = "Time: 0";
+        $("#time_counter").html("Time: 0");
     },
     clearMoves: function () {
         puzzle.gameMoves = 0;
-        document.getElementById("moves_counter").textContent = "Moves: 0";
+        $("#moves_counter").html("Moves: 0");
     },
     addMove: function () {
         ++puzzle.gameMoves;
-        document.getElementById("moves_counter").textContent = "Moves: " + puzzle.gameMoves;
+        $("#moves_counter").html("Moves: " + puzzle.gameMoves);
     },
     startGame: function () {
         puzzle.gameStart = true;
@@ -25,22 +28,15 @@ var puzzle = {
         puzzle.ableToClick = true;
         puzzle.gameTimer = setInterval(function () {
             puzzle.gameTime += 1;
-            document.getElementById("time_counter").textContent = "Time: " + Math.floor(puzzle.gameTime);
+            $("#time_counter").html("Time: " + puzzle.gameTime);
         }, 1000);
     },
     winGame: function () {
         puzzle.gameStart = false;
         puzzle.ableToClick = true;
         clearInterval(puzzle.gameTimer);
-        var hoverMessage = document.createElement("div");
-        hoverMessage.id = "hover_message";
-        hoverMessage.innerHTML = "<div>You Win!<br/>You used " + puzzle.gameTime + " seconds<br/>with " + puzzle.gameMoves + " moves</div>";
-        document.getElementById("main_content").className = "blur";
-        hoverMessage.onclick = function () {
-            document.getElementById("main_content").className = "no_blur";
-            document.body.removeChild(document.getElementById("hover_message"));
-        }
-        document.body.appendChild(hoverMessage);
+        showHoverMessage("You Win")
+        $("#hover_message").click(hideHoverMessage);
     },
     stopGame: function () {
         puzzle.gameStart = false;
@@ -48,59 +44,71 @@ var puzzle = {
         clearInterval(puzzle.gameTimer);
     }
 }
-var dx = [-1, 0, 1, 0];
-var dy = [0, -1, 0, 1];
-var fac = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000];
 
-var initPuzzle = function () {
-    var puzzleContent = document.getElementById("puzzle_content");
-    puzzle.matrix[0] = 0; // set the first block to be empty
-    for (var i = 1; i < 16; ++i) {
-        var newBlock = document.createElement("canvas");
-        newBlock.className = "puzzle_block";
-        newBlock.id = i;
-        newBlock.position = i;
-        // 改变dom位置
-        newBlock.updatePosition = function () {
-            this.style.left = (80 * (this.position % 4)) + "px";
-            this.style.top = (80 * Math.floor(this.position / 4)) + "px";
-        }
-        // moveToPosition 将本滑块与对应位置滑块交换
-        newBlock.moveToPosition = function (newPosition) {
-            if (puzzle.matrix[newPosition] != "0") {
-                var otherBlock = document.getElementById(puzzle.matrix[newPosition]);
-                otherBlock.position = this.position;
-                otherBlock.updatePosition();
-            }
-            puzzle.matrix[this.position] = puzzle.matrix[newPosition];
-            puzzle.matrix[newPosition] = parseInt(this.id);
-            this.position = newPosition;
-            this.updatePosition();
-        }
-        newBlock.updatePosition();
-        newBlock.onclick = function (event) {
-            if (puzzle.ableToClick) {
-                for (var i = 0; i < 4; ++i) {
-                    var x = this.position % 4 + dx[i], y = Math.floor(this.position / 4) + dy[i];
-                    var j = x + 4 * y;
-                    if (x >= 0 && x < 4 && y >= 0 && y < 4 && puzzle.matrix[j] == '0') {
-                        if (puzzle.gameStart) {
-                            puzzle.addMove();
-                        }
-                        this.moveToPosition(j);
-                        if (puzzle.gameStart && contor(puzzle.matrix) == 0) puzzle.winGame();
-                        event.stopPropagation(); // 停止冒泡
-                        break;
-                    }
-                }
+function updatePosition(newPosition) {
+    $(this).removeClass("puzzle_row_" + Math.floor(this.position / 4));
+    $(this).removeClass("puzzle_col_" + this.position % 4);
+    this.position = newPosition;
+    $(this).addClass("puzzle_row_" + Math.floor(this.position / 4));
+    $(this).addClass("puzzle_col_" + this.position % 4);
+}
+
+function moveToPosition(newPosition) {
+    puzzle.blocks[puzzle.matrix[newPosition]].updatePosition(this.position);
+    puzzle.matrix[this.position] = puzzle.matrix[newPosition];
+    puzzle.matrix[newPosition] = parseInt(this.id);
+    this.updatePosition(newPosition);
+}
+
+function getNeighbour(position) {
+    var dx = [-1, 0, 1, 0], dy = [0, -1, 0, 1];
+    var neighbour = new Array(), x, y;
+    for (var i = 0; i < 4; ++i) {
+        x = position % 4 + dx[i], y = Math.floor(position / 4) + dy[i];
+        if (x >= 0 && x < 4 && y >= 0 && y < 4) neighbour.push(x + 4 * y);
+    }
+    return neighbour;
+}
+
+function handleOnClick(event) {
+    if (puzzle.ableToClick) {
+        var neighbours = this.getNeighbour();
+        for (var index in neighbours) {
+            if (puzzle.matrix[neighbours[index]] == "0") {
+                if (puzzle.gameStart) puzzle.addMove();
+                this.moveToPosition(neighbours[index]);
+                if (puzzle.gameStart && contor(puzzle.matrix) == 0) puzzle.winGame();
+                event.stopPropagation(); // 停止冒泡
+                break;
             }
         }
-        puzzle.matrix[i] = i;
-        puzzleContent.appendChild(newBlock);
     }
 }
 
-drawPuzzle = function (imageName) {
+function puzzleBlock() {
+    var newBlock = document.createElement("canvas");
+    newBlock.className = "puzzle_block";
+    newBlock.position = 0;
+    newBlock.updatePosition = updatePosition;
+    newBlock.moveToPosition = moveToPosition;
+    newBlock.getNeighbour = function () { return getNeighbour(this.position); }
+    newBlock.onclick = handleOnClick;
+    return newBlock;
+}
+
+// 初始化puzzle
+function initPuzzle() {
+    for (var i = 0; i < 16; ++i) {
+        var newBlock = puzzleBlock(i);
+        newBlock.id = newBlock.position = i;
+        newBlock.updatePosition(i);
+        puzzle.matrix[i] = i;
+        puzzle.blocks[i] = newBlock;
+        $("#puzzle_content").append(newBlock);
+    }
+}
+
+function drawPuzzle(imageName) {
     var image = new Image();
     image.src = imageName;
     image.onload = function () {
@@ -112,37 +120,34 @@ drawPuzzle = function (imageName) {
     image.onload();
 }
 
-var breakPuzzle = function () {
+// 随机选择一个格子移动
+function randomMove(cur, pre, times) {
+    if (times < puzzle.randomTimes) {
+        var possiblePostion = puzzle.blocks[0].getNeighbour();
+        possiblePostion.splice(possiblePostion.indexOf(pre), 1); // 删除上一次移动的方块
+        var pos = possiblePostion[Math.floor(Math.random() * possiblePostion.length)]
+        puzzle.blocks[puzzle.matrix[pos]].moveToPosition(cur);
+        setTimeout(function () {
+            randomMove(pos, cur, times + 1);
+        }, puzzle.randomDelay);
+    } else {
+        puzzle.startGame();
+    }
+}
+
+// 打乱puzzle
+function breakPuzzle() {
     if (puzzle.ableToClick) {
         puzzle.stopGame();
         puzzle.ableToClick = false;
         var zero = puzzle.matrix.indexOf(0);
-        (function randomMove(cur, pre, times) {
-            if (times < puzzle.randomTimes) {
-                var possiblePostion = new Array();
-                for (var i = 0; i < 4; ++i) {
-                    var x = cur % 4 + dx[i], y = Math.floor(cur / 4) + dy[i];
-                    var pos = x + 4 * y;
-                    if (x >= 0 && x < 4 && y >= 0 && y < 4) possiblePostion.push(pos);
-                }
-                var find = possiblePostion.indexOf(pre);
-                if (find != -1) {
-                    possiblePostion.splice(find, 1);
-                }
-                var pos = possiblePostion[Math.floor(Math.random() * possiblePostion.length)]
-                document.getElementById(puzzle.matrix[pos]).moveToPosition(cur);
-                setTimeout(function () {
-                    randomMove(pos, cur, times + 1);
-                }, 10);
-            } else {
-                puzzle.startGame();
-            }
-        })(zero, zero, 0);
+        randomMove(zero, zero, 0);
     }
 }
 
-// 康拓展开压缩状态
-contor = function (array) {
+// 康拓展开
+function contor(array) {
+    var fac = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000];
     var rst = 0, t = 0;
     for (var i = 0; i < array.length; ++i) {
         t = 0;
@@ -154,88 +159,83 @@ contor = function (array) {
     return rst;
 }
 
-solvePuzzle = function () {
-    if (puzzle.ableToClick) {
-        puzzle.stopGame();
-        puzzle.ableToClick = false;
-        var hoverMessage = document.createElement("div");
-        hoverMessage.id = "hover_message";
-        hoverMessage.innerHTML = "<div><div class=\"loading_icon\"></div>Solving...</div>";
-        document.getElementById("main_content").className = "blur";
-        document.body.appendChild(hoverMessage);
-        // 搜索状态
-        statu = function (matrix, step, depth, zeroIndex) {
-            this.matrix = matrix;
-            this.step = step;
-            this.depth = depth;
-            this.zeroIndex = zeroIndex;
+// A* 搜索状态
+function statu(matrix, steps, depth, zeroIndex) {
+    this.matrix = matrix;
+    this.steps = steps;
+    this.depth = depth;
+    this.zeroIndex = zeroIndex;
+}
+
+// A* 估值函数
+function value(statu) {
+    var rst = 0;
+    for (var x = 0; x < 4; ++x) {
+        for (var y = 0; y < 4; ++y) {
+            var pos = statu.matrix[x + 4 * y];
+            // 到目标位置的曼哈顿距离
+            rst += Math.abs((pos % 4) - x) + Math.abs(Math.floor(pos / 4) - y);
         }
-        // A*启发函数
-        value = function (statu) {
-            var rst = 0;
-            for (var x = 0; x < 4; ++x) {
-                for (var y = 0; y < 4; ++y) {
-                    var pos = statu.matrix[x + 4 * y];
-                    // 到目标位置的曼哈顿距离
-                    rst += Math.abs((pos % 4) - x) + Math.abs(Math.floor(pos / 4) - y);
-                }
-            }
-            return -statu.depth - 4 * rst; // 曼哈顿距离乘上一定系数来改善启发函数
-        }
-        var sta = new statu(puzzle.matrix, new Array(), 0, puzzle.matrix.indexOf(0)); // 初始状态
-        // A* 寻路
+    }
+    return -statu.depth - 4 * rst; // 曼哈顿距离乘上一定系数来改善启发函数
+}
+
+// 将steps中存储的步骤作用于puzzle
+function solveMove(steps, index, pre) {
+    if (index < steps.length) {
+        puzzle.blocks[puzzle.matrix[steps[index]]].moveToPosition(pre);
         setTimeout(function () {
-            var close = new Set();
-            var open = new PriorityQueue();
-            open.insert(value(sta), sta);
-            close.add(contor(sta.matrix));
-            while (!open.empty()) {
-                var curStatu = open.pop();
-                var cur = curStatu.entry;
-                if (contor(cur.matrix) == 0) { // 如果找到解法
-                    console.log(close.size, cur.depth);
-                    document.getElementById("main_content").className = "no_blur";
-                    document.body.removeChild(document.getElementById("hover_message"));
-                    // 根据寻路结果操作
-                    var j = puzzle.matrix.indexOf(0);
-                    (function solveMove(i) {
-                        if (i < cur.step.length) {
-                            document.getElementById(puzzle.matrix[cur.step[i]]).moveToPosition(j);
-                            j = cur.step[i];
-                            setTimeout(function () {
-                                solveMove(i + 1);
-                            }, 40);
-                        } else {
-                            puzzle.ableToClick = true;
-                        }
-                    })(0);
-                    return;
-                }
-                var index = cur.zeroIndex;
-                for (var i = 0; i < 4; ++i) {
-                    var x = index % 4 + dx[i], y = Math.floor(index / 4) + dy[i];
-                    var nindex = x + 4 * y;
-                    if (x >= 0 && x < 4 && y >= 0 && y < 4) {
-                        var matrix = cur.matrix.slice(0);
-                        var temp = matrix[index];
-                        matrix[index] = matrix[nindex];
-                        matrix[nindex] = temp;
-                        var contorNumber = contor(matrix);
-                        if (!close.has(contorNumber)) {
-                            var step = cur.step.slice(0); // 复制step
-                            step.push(nindex);
-                            var nstatu = new statu(matrix, step, cur.depth + 1, nindex);
-                            open.insert(value(nstatu), nstatu);
-                            close.add(contorNumber);
-                        }
-                    }
-                }
-            }
-        });
+            solveMove(steps, index + 1, steps[index]);
+        }, puzzle.solveDelay);
+    } else {
+        puzzle.ableToClick = true;
     }
 }
 
-handleFileSelect = function (event) {
+// A*
+function AStar(sta) {
+    var close = new Set(); // 已搜索状态
+    var open = new PriorityQueue(); // 待搜索状态
+    open.insert(value(sta), sta);
+    close.add(contor(sta.matrix));
+    while (!open.empty()) {
+        var curStatu = open.pop();
+        var cur = curStatu.entry;
+        if (contor(cur.matrix) == 0) { // 如果找到解法
+            hideHoverMessage();
+            solveMove(cur.steps, 0, puzzle.matrix.indexOf(0));
+            return;
+        }
+        var neighbours = getNeighbour(cur.zeroIndex);
+        for (var i in neighbours) {
+            var matrix = cur.matrix.slice(0); // 复制matrix
+            var temp = matrix[cur.zeroIndex]; matrix[cur.zeroIndex] = matrix[neighbours[i]]; matrix[neighbours[i]] = temp; // 交换位置
+            var contorNumber = contor(matrix);
+            if (!close.has(contorNumber)) {
+                close.add(contorNumber);
+                var steps = cur.steps.slice(0); // 复制steps
+                steps.push(neighbours[i]);
+                var nstatu = new statu(matrix, steps, cur.depth + 1, neighbours[i]);
+                open.insert(value(nstatu), nstatu);
+            }
+        }
+    }
+}
+
+function solvePuzzle() {
+    if (puzzle.ableToClick) {
+        puzzle.stopGame();
+        puzzle.ableToClick = false;
+        showHoverMessage("<div class=\"loading_icon\"></div>Solving...");
+        var sta = new statu(puzzle.matrix, new Array(), 0, puzzle.matrix.indexOf(0)); // 初始状态
+        AStar(sta); // A* 寻路
+    }
+}
+
+
+// 获取拖入图片
+function handleFileSelect(event) {
+    event = event.originalEvent;
     event.stopPropagation();
     event.preventDefault();
     var files = event.dataTransfer.files;
@@ -245,21 +245,31 @@ handleFileSelect = function (event) {
     }
 }
 
-handleDragOver = function (event) {
+// 改变拖入时的效果
+function handleDragOver(event) {
+    event = event.originalEvent;
     event.stopPropagation();
     event.preventDefault();
-    event.dataTransfer.dropEffect = 'copy';
+    event.dataTransfer.dropEffect = "copy";
+}
+
+// 在最上层显示message
+function showHoverMessage(message) {
+    $("#message").html(message);
+    $("#hover_message").show();
+    $("#main_content").addClass("blur");
+}
+
+function hideHoverMessage() {
+    $("#hover_message").hide();
+    $("#main_content").removeClass("blur");
 }
 
 window.onload = function () {
-    document.getElementById("break").onclick = breakPuzzle;
-    document.getElementById("solve").onclick = solvePuzzle;
-    var puzzleContent = document.getElementById("puzzle_content");
-    var dropZone = document.getElementById("drop_zone");
-    puzzleContent.addEventListener("dragover", handleDragOver, false);
-    puzzleContent.addEventListener("drop", handleFileSelect, false);
-    dropZone.addEventListener("dragover", handleDragOver, false);
-    dropZone.addEventListener("drop", handleFileSelect, false);
+    $("#break").click(breakPuzzle);
+    $("#solve").click(solvePuzzle);
+    $("#drop_zone, #puzzle_content").bind({ "drop": handleFileSelect, "dragover": handleDragOver });
     initPuzzle();
     drawPuzzle("images/panda.jpg");
+    $("#hover_message").hide();
 }
